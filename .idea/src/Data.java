78887c0e6327b100e7a16bfe475ceb3695f92d2d
Xlich21 <alex.fenichiu@gmail.com>
2018-11-12 20:24:46 +0200

    import

---
+++ .idea/src/Data.java
@@ -0,0 +1,22 @@
+public class Data {
+    public static void main(String[] args) {
+        int myAge = 32;
+        float weight = 60.5f;
+        double height = 356.6;
+        long distance = 1234124151325153215l;
+        char gender = 'm';
+        char test = 9;
+        boolean isStudent = false;
+        boolean hasJob = true;
+        int test2 = (int) weight;
+        System.out.print(test);
+        System.out.print(test2);
+
+    }
+
+
+
+
+
+
+}
